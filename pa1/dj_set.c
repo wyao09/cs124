f7bcb92d42d460a79fff49c8aabf1cb2b0e162b0
Willie Yao <wyao09@gmail.com>
2012-02-26 18:03:30 -0500

    styling changes

--- pa1/dj_set.c
+++ pa1/dj_set.c
@@ -4,62 +4,62 @@
 #include "dj_set.h"
 
 struct node {
-    struct node *prev;
-    int id;
-    int rank;
+  struct node *prev;
+  int id;
+  int rank;
 };
 
 node *makeset(int x)
 {
-    node *new = (node *) malloc(sizeof(node));
-    new->id = x;
-    new->prev = new;
-    new->rank = 0;
+  node *new = (node *) malloc(sizeof(node));
+  new->id = x;
+  new->prev = new;
+  new->rank = 0;
     
-    return new;
+  return new;
 }
 
 node *find(node *x)
 {
-    if (x != x->prev)
+  if (x != x->prev)
     {
-        //path compression - otherwise, return find(x->prev)
-        x->prev = find(x->prev);
-        return x->prev;
+      //path compression - otherwise, return find(x->prev)
+      x->prev = find(x->prev);
+      return x->prev;
     }
-    return x;
+  return x;
 }
 
 void link(node *x, node *y)
 {
-    if (x->rank > y->rank)
+  if (x->rank > y->rank)
     {
-        link(y,x);
-        return;
+      link(y,x);
+      return;
     }
-    if (x->rank == y->rank)
-        y->rank++;
-    x->prev = y;
+  if (x->rank == y->rank)
+    y->rank++;
+  x->prev = y;
 }
 
 void dj_union(node *x, node *y)
 {
-    link(find(x),find(y));
+  link(find(x),find(y));
 }
 
 /*
-int main(int argc, char **argv)
-{
-    node *a = makeset(0);
-    node *b = makeset(1);
-    node *c = makeset(2);
+  int main(int argc, char **argv)
+  {
+  node *a = makeset(0);
+  node *b = makeset(1);
+  node *c = makeset(2);
     
-    dj_union(b,a);
-    dj_union(b,c);
+  dj_union(b,a);
+  dj_union(b,c);
     
-    printf("%d, %d\n",find(a)->id, find(a)->rank);
-    printf("%d, %d\n",find(b)->id, find(b)->rank);
+  printf("%d, %d\n",find(a)->id, find(a)->rank);
+  printf("%d, %d\n",find(b)->id, find(b)->rank);
     
-    return 1;
-}
+  return 1;
+  }
 */
--- pa1/krustal.c
+++ pa1/krustal.c
@@ -1,10 +1,13 @@
-//
-//  krustal.c
-//  
-//
-//  Created by Aidan Daly on 2/25/12.
-//  Copyright 2012 __MyCompanyName__. All rights reserved.
-//
+/*
+ * krustal.c - 
+ *
+ * usage: randmst 0 <numpoints> <numtrials> <dimension>
+ *
+ * assignment: cs124 pa1
+ *
+ * authors: Aidan Daly <aidan.daly@college.harvard.edu>
+ *          Willie Yao <wyao13@college.harvard.edu>
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,45 +17,46 @@
 //This is the 0 dimension case with random weighted edges between each node
 int krustal_rand_wts (int numpoints)
 {
-    double weights[numpoints][numpoints];
-    node *vertices[numpoints];
+  //Use integer values for faster run time?
+  double weights[numpoints][numpoints];
+  node *vertices[numpoints];
     
-    int i, j;
-    srand(time(NULL)); //seeds random generator with current system time
+  int i, j;
+  srand(time(NULL)); //seeds random generator with current system time
     
-    for (i=0; i<numpoints; i++)
+  for (i=0; i<numpoints; i++)
     {
-        vertices[i] = makeset(i);
-        for (j=0; j<numpoints; j++)
-            weights[i][j] = ((double)rand()/ (double)(RAND_MAX));
+      vertices[i] = makeset(i);
+      for (j=0; j<numpoints; j++)
+	weights[i][j] = ((double)rand()/ (double)(RAND_MAX));
     }
     
-    //Sort edges
+  //Sort edges
     
-    //for edges in increasing order
+  //for edges in increasing order
 }
 
 int main (int argc, char **argv)
 {
-    if (argc != 5) {
-        printf("Usage: 0 numpoints numtrials dimension\n");
-        return 0;
+  if (argc != 5) {
+    printf("Usage: 0 numpoints numtrials dimension\n");
+    return 0;
+  }
+    
+  int flag = atoi(argv[1]);
+  int numpoints = atoi(argv[2]);
+  int numtrials = atoi(argv[3]);
+  int dimension = atoi(argv[4]);
+    
+  if (numpoints <= 0 || numtrials <= 0 || dimension < 0)
+    {
+      printf("numpoints, numtrials, dimension must be non-negative integers");
+      return 0;
     }
     
-    int flag = atoi(argv[1]);
-    int numpoints = atoi(argv[2]);
-    int numtrials = atoi(argv[3]);
-    int dimension = atoi(argv[4]);
-    
-    if (numpoints <= 0 || numtrials <= 0 || dimension < 0)
+  // Case for Random Weights
+  if (dimension == 0)
     {
-        printf("numpoints, numtrials, dimension must be non-negative integers");
-        return 0;
-    }
-    
-    // Case for Random Weights
-    if (dimension == 0)
-    {
-        krustal_rand_wts(numpoints);
+      krustal_rand_wts(numpoints);
     }
 }
