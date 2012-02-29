f716e3befaebe8fc480b8552e13ef4b6921ea9d9
Willie Yao <wyao09@gmail.com>
2012-02-28 23:49:19 -0500

    shell script

---
+++ pa1/run.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+echo "./randmst 0 16 5 0"
+./randmst 0 16 5 0
+echo "./randmst 0 32 5 0"
+./randmst 0 32 5 0
+echo "./randmst 0 64 5 0"
+./randmst 0 64 5 0
+echo "./randmst 0 128 5 0"
+./randmst 0 128 5 0
+echo "./randmst 0 256 5 0"
+./randmst 0 256 5 0
+echo "./randmst 0 512 5 0"
+./randmst 0 512 5 0
+echo "./randmst 0 1024 5 0"
+./randmst 0 1024 5 0
+echo "./randmst 0 2048 5 0"
+./randmst 0 2048 5 0
+echo "./randmst 0 4096 5 0"
+./randmst 0 4096 5 0
+echo "./randmst 0 8192 5 0"
+./randmst 0 8192 5 0
+echo "./randmst 0 16384 5 0"
+./randmst 0 16384 5 0
+echo "./randmst 0 32678 5 0"
+./randmst 0 32678 5 0
+echo " "
+echo "./randmst 0 16 5 2"
+./randmst 0 16 5 2
+echo "./randmst 0 32 5 2"
+./randmst 0 32 5 2
+echo "./randmst 0 64 5 2"
+./randmst 0 64 5 2
+echo "./randmst 0 128 5 2"
+./randmst 0 128 5 2
+echo "./randmst 0 256 5 2"
+./randmst 0 256 5 2
+echo "./randmst 0 512 5 2"
+./randmst 0 512 5 2
+echo "./randmst 0 1024 5 2"
+./randmst 0 1024 5 2
+echo "./randmst 0 2048 5 2"
+./randmst 0 4048 5 2
+echo "./randmst 0 2096 5 2"
+./randmst 0 4096 5 2
+echo "./randmst 0 8192 5 2"
+./randmst 0 8192 5 2
+echo "./randmst 0 16384 5 2"
+./randmst 0 16384 5 2
+echo "./randmst 0 32678 5 2"
+./randmst 0 32678 5 2
+echo " "
+echo "./randmst 0 16 5 3"
+./randmst 0 16 5 3
+echo "./randmst 0 32 5 3"
+./randmst 0 32 5 3
+echo "./randmst 0 64 5 3"
+./randmst 0 64 5 3
+echo "./randmst 0 128 5 3"
+./randmst 0 128 5 3
+echo "./randmst 0 256 5 3"
+./randmst 0 256 5 3
+echo "./randmst 0 512 5 3"
+./randmst 0 512 5 3
+echo "./randmst 0 1024 5 3"
+./randmst 0 1024 5 3
+echo "./randmst 0 2048 5 3"
+./randmst 0 2048 5 3
+echo "./randmst 0 4096 5 3"
+./randmst 0 4096 5 3
+echo "./randmst 0 8192 5 3"
+./randmst 0 8192 5 3
+echo "./randmst 0 16384 5 3"
+./randmst 0 16384 5 3
+echo "./randmst 0 32678 5 3"
+./randmst 0 32678 5 3
+echo " "
+echo "./randmst 0 16 5 4"
+./randmst 0 16 5 4
+echo "./randmst 0 32 5 4"
+./randmst 0 32 5 4
+echo "./randmst 0 64 5 4"
+./randmst 0 64 5 4
+echo "./randmst 0 128 5 4"
+./randmst 0 128 5 4
+echo "./randmst 0 256 5 4"
+./randmst 0 256 5 4
+echo "./randmst 0 512 5 4"
+./randmst 0 512 5 4
+echo "./randmst 0 1024 5 4"
+./randmst 0 1024 5 4
+echo "./randmst 0 2048 5 4"
+./randmst 0 2048 5 4
+echo "./randmst 0 4096 5 4"
+./randmst 0 4096 5 4
+echo "./randmst 0 8192 5 4"
+./randmst 0 8192 5 4
+echo "./randmst 0 16384 5 4"
+./randmst 0 16384 5 4
+echo "./randmst 0 32678 5 4"
+./randmst 0 32678 5 4
