2044b0fd5362bad0071037667d67a17db3883769
Willie Yao <wyao09@gmail.com>
2012-03-01 06:15:11 -0500

    finalized

---
+++ pa1/bin/test0t.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+./randmst 2 16 5 0
+./randmst 2 32 5 0
+./randmst 2 64 5 0
+./randmst 2 128 5 0
+./randmst 2 256 5 0
+./randmst 2 512 5 0
+./randmst 2 1024 5 0
+./randmst 2 2048 5 0
+./randmst 2 4096 5 0
+./randmst 2 8192 5 0
+./randmst 2 16384 5 0
+./randmst 2 32678 5 0
---
+++ pa1/bin/test4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+./randmst 1 16 5 4
+./randmst 1 32 5 4
+./randmst 1 64 5 4
+./randmst 1 128 5 4
+./randmst 1 256 5 4
+./randmst 1 512 5 4
+./randmst 1 1024 5 4
+./randmst 1 2048 5 4
+./randmst 1 4096 5 4
+./randmst 1 8192 5 4
+./randmst 1 16384 5 4
+./randmst 1 32678 5 4
