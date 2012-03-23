f0e97cf3e43200d17385058fcdd71b6c00748b2f
Willie Yao <wyao09@gmail.com>
2012-03-23 02:20:51 -0400

    started padding 0s

--- pa2/strassen.c
+++ pa2/strassen.c
@@ -23,6 +23,12 @@
 #define IV 4
 #define INT 12
 
+int pow_2[33] = {1,2,4,8,16,32,64,128,256,512,1024,2048,
+		 4096,8192,16384,32768,65536,131072,262144,
+		 524288,1048576,2097152,4194304,8388608,
+		 16777216,33554432,67108864,134217728,268435456,
+		 536870912,1073741824,2147483648,4294967296};
+
 int main(int argc, char **argv){
   if (argc != 4){
     printf("usage: ./strassen 0 <dimension> <inputﬁle>\n");
@@ -36,6 +42,9 @@ int main(int argc, char **argv){
   int **a = m_malloc(dim);
   int **b = m_malloc(dim);
   int **c = m_malloc(dim);
+
+  // need to round dimension size to pow of 2 and pad zeros
+
 
 
   // file I/O
