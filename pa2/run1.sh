4f08229b0dbe4e3405aa95988ae7ebe0ec618d88
Aidan Daly <aidandaly@dhcp-140-247-149-101.fas.harvard.edu>
2012-03-28 15:42:42 -0400

    added graphs for runtime and cutoff experiments

---
+++ pa2/run1.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+echo "Strassen"
+#echo "./strassen 1 100 r"
+./strassen 1 100 r
+#echo "./strassen 1 200 r"
+./strassen 1 200 r
+#echo "./strassen 1 300 r"
+./strassen 1 300 r
+#echo "./strassen 1 400 r"
+./strassen 1 400 r
+#echo "./strassen 1 500 r"
+./strassen 1 500 r
+#echo "./strassen 1 600 r"
+./strassen 1 600 r
+#echo "./strassen 1 700 r"
+./strassen 1 700 r
+#echo "./strassen 1 800 r"
+./strassen 1 800 r
+#echo "./strassen 1 900 r"
+./strassen 1 900 r
+#echo "./strassen 1 1000 r"
+./strassen 1 1000 r
+#echo "./strassen 1 1100 r"
+./strassen 1 1100 r
+#echo "./strassen 1 1200 r"
+./strassen 1 1200 r
+#echo "./strassen 1 1300 r"
+./strassen 1 1300 r
+#echo "./strassen 1 1400 r"
+./strassen 1 1400 r
+#echo "./strassen 1 1500 r"
+./strassen 1 1500 r
+#echo "./strassen 1 1600 r"
+./strassen 1 1600 r
+#echo "./strassen 1 1700 r"
+./strassen 1 1700 r
+#echo "./strassen 1 1800 r"
+./strassen 1 1800 r
+#echo "./strassen 1 1900 r"
+./strassen 1 1900 r
+#echo "./strassen 1 2000 r"
+./strassen 1 2000 r
+#echo "./strassen 1 2100 r"
+./strassen 1 2100 r
+#echo "./strassen 1 2200 r"
+./strassen 1 2200 r
+#echo "./strassen 1 2300 r"
+./strassen 1 2300 r
+#echo "./strassen 1 2400 r"
+./strassen 1 2400 r
+#echo "./strassen 1 2500 r"
+./strassen 1 2500 r
+#echo "./strassen 1 2600 r"
+./strassen 1 2600 r
+#echo "./strassen 1 2700 r"
+./strassen 1 2700 r
+#echo "./strassen 1 2800 r"
+./strassen 1 2800 r
+#echo "./strassen 1 2900 r"
+./strassen 1 2900 r
+#echo "./strassen 1 3000 r"
+./strassen 1 3000 r
+#echo "./strassen 1 3100 r"
+./strassen 1 3100 r
+#echo "./strassen 1 3200 r"
+./strassen 1 3200 r
+#echo "./strassen 1 3300 r"
+./strassen 1 3300 r
+#echo "./strassen 1 3400 r"
+./strassen 1 3400 r
+#echo "./strassen 1 3500 r"
+./strassen 1 3500 r
+#echo "./strassen 1 3600 r"
+./strassen 1 3600 r
+#echo "./strassen 1 3700 r"
+./strassen 1 3700 r
+#echo "./strassen 1 3800 r"
+./strassen 1 3800 r
+#echo "./strassen 1 3900 r"
+./strassen 1 3900 r
+#echo "./strassen 1 4000 r"
+./strassen 1 4000 r
