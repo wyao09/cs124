ad9ba5bf8e7273477a411ca8f6e14c9668fb57fb
Willie Yao <wyao09@gmail.com>
2012-04-01 18:56:03 -0400

    switched to mpmath

--- ps4/composite.py
+++ ps4/composite.py
@@ -1,10 +1,11 @@
 import math
 import sys
 import random
+from mpmath import mp
 
-n = long(sys.argv[1]) # odd integer to be tested for primality
-s = long(sys.argv[2]) # n-1 as 2s*d
-d = long(sys.argv[3])
+n = mp.mpf(sys.argv[1]) # odd integer to be tested for primality
+s = int(sys.argv[2]) # n-1 as 2s*d
+d = mp.mpf(sys.argv[3])
 k = int(sys.argv[4]) # parameter that determines the accuracy of the test
 
 
@@ -13,15 +14,15 @@ k = int(sys.argv[4]) # parameter that determines the accuracy of the test
 
 def composite():
     for i in range(0, k):
-        a = random.randint(2,n-2)
-        x = math.pow(a,d)
-        x = x % n
-        if x != 1 and x != n-1:
+        a = mp.mpf(random.randint(2,n-2))
+        x = mp.power(a,d)
+        x = mp.fmod(x,n)
+        if x != 1 and x != n-1:#
             for r in range(i, s-1):
-                x = pow(x,2) % n
-                if x == 1:
+                x = mp.fmod(mp.power(x,2),n)
+                if x == 1:#
                     return a
-                if x != n-1:
+                if x != n-1:#
                     return a
     return -1
     
