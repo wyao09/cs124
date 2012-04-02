292d7cf9e0648da98fdb537b1e3cd32b4d4b1e84
Willie Yao <wyao09@gmail.com>
2012-04-01 20:08:07 -0400

    works for 294409, but not 636127

--- ps4/composite.py
+++ ps4/composite.py
@@ -3,27 +3,33 @@ import sys
 import random
 from mpmath import mp
 
-n = mp.mpf(sys.argv[1]) # odd integer to be tested for primality
+n = int(sys.argv[1]) # odd integer to be tested for primality
+"""
 s = int(sys.argv[2]) # n-1 as 2s*d
 d = mp.mpf(sys.argv[3])
-k = int(sys.argv[4]) # parameter that determines the accuracy of the test
-
+"""
+k = int(sys.argv[2]) # parameter that determines the accuracy of the test
+s = 0
+d = 0
 
 # find s and d first
-
+for i in range(0, 20):
+    x = mp.fdiv((n-1), mp.power(2,i))
+    if x - long(x) == 0:
+        s = i
+        d = int(x)
 
 def composite():
     for i in range(0, k):
-        a = mp.mpf(random.randint(2,n-2))
-        x = mp.power(a,d)
-        x = mp.fmod(x,n)
+        a = random.randint(2,n-2)
+        x = mp.fmod(mp.power(a,d),n)
         if x != 1 and x != n-1:#
             for r in range(i, s-1):
                 x = mp.fmod(mp.power(x,2),n)
                 if x == 1:#
-                    return a
+                    return a, s, d
                 if x != n-1:#
-                    return a
-    return -1
+                    return a, s, d
+    return -1, s, d
     
 print composite()
